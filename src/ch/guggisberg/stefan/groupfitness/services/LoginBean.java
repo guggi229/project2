1bda0ecae2b57841195ecc585167e81d7942cd50
DESKTOP-IG3FFMH\guggi229 <guggs2@bfh.ch>
2017-09-23 22:47:08 +0200

    Loginform checkt Daten mit DB.

--- src/ch/guggisberg/stefan/groupfitness/services/LoginBean.java
+++ src/ch/guggisberg/stefan/groupfitness/services/LoginBean.java
@@ -2,41 +2,74 @@ package ch.guggisberg.stefan.groupfitness.services;
 
 import java.io.Serializable;
 
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
 
+
+
+/**
+ * 
+ * @author guggi229
+ * 
+ * This bean handle the login.
+ *
+ */
 @ManagedBean(name="loginBean")
 @SessionScoped
 public class LoginBean implements Serializable  {
 	private static final long serialVersionUID = -3070846601605974845L;
+	
+	// DB Verbindung
+	@PersistenceContext
+	private EntityManager em;
+	@Resource
+	private UserTransaction ut;
+	
 	// Logindaten
 	private String  email;
 	private String pass;
 	private boolean loggedIn;
 	
-	public String checkLogin(){
+	/**
+	 * Check the login Data
+	 * @return
+	 */
+	public void checkLogin(){
+		Integer result=0;
+		Query query = em.createNativeQuery("Select Userid from groupfitness.user u WHERE u.UserEmail='" + email + "' AND u.UserPassword='" + pass + "'");
+		//Query query = em.createNativeQuery("Select Userid from groupfitness.user u WHERE UserEmail='stefan@gmx.ch' AND u.UserPassword='123';");
+
+		System.out.println("check");
+		try {
+			result =  (Integer) query.getSingleResult();
+			System.out.println("query ausgeführt");
+			if (result.intValue()>0){
+				//user = em.find(User2.class, result.longValue());
+				System.out.println("Logged in");
+				loggedIn=true;
+			
+			}
+		} catch (NoResultException e) {
+			loggedIn=false;
+			System.out.println("not logged in");
 		
-	System.out.println("check");
-	
-	return null;
+		}
+
+		
 	}
 	
-	
+	// Getter and Setter
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
-		System.out.println("*******************************");
-		System.out.println("*******************************");
-		System.out.println("*******************************");
-		System.out.println("*******************************");
-		System.out.println("*******************************");
-		System.out.println("*******************************");
-		System.out.println(email);
 		this.email = email;
-	}
-	public String getPass() {
-		return pass;
 	}
 	public void setPass(String pass) {
 		this.pass = pass;
@@ -44,11 +77,14 @@ public class LoginBean implements Serializable  {
 	public boolean isLoggedIn() {
 		return loggedIn;
 	}
+
+	public String getPass() {
+		return pass;
+	}
+
 	public void setLoggedIn(boolean loggedIn) {
 		this.loggedIn = loggedIn;
 	}
-	
-	
 	
 
 }
