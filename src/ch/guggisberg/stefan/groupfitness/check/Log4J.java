bd2d3cc4d0f6f1cfe11df039fa7afa404b0baddb
DESKTOP-IG3FFMH\guggi229 <guggs2@bfh.ch>
2017-09-24 12:05:05 +0200

    Log4J eingebaut und getestet.

---
+++ src/ch/guggisberg/stefan/groupfitness/check/Log4J.java
@@ -0,0 +1,12 @@
+package ch.guggisberg.stefan.groupfitness.check;
+
+import org.apache.log4j.Logger;
+
+public class Log4J {
+
+	private static Logger log = Logger.getLogger(Log4J.class);
+	public static void main(String[] args) {
+		
+		log.info("Test Message");
+	}
+}
