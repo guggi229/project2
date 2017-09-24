63124cd235a57cc4bc12c6fe25a1ff808015c206
DESKTOP-IG3FFMH\guggi229 <guggs2@bfh.ch>
2017-09-24 21:24:45 +0200

    Login Filter hinzugefügt

---
+++ src/ch/guggisberg/stefan/groupfitness/filters/LoginFilter.java
@@ -0,0 +1,42 @@
+package ch.guggisberg.stefan.groupfitness.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch.guggisberg.stefan.groupfitness.services.LoginBean;
+
+public class LoginFilter implements Filter {
+
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		 HttpServletRequest req = (HttpServletRequest) request;
+		 LoginBean loginBean = (LoginBean) req.getSession().getAttribute("loginBean");
+		if(loginBean==null || !loginBean.isLogged()) {
+			String contextPath = ((HttpServletRequest)request).getContextPath();
+			((HttpServletResponse)response).sendRedirect(contextPath+"/login.xhtml");
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// da gibts nichts zu tun! :-)
+
+	}
+	@Override
+	public void destroy() {
+		// da gibts nichts zu tun! :-)
+
+	}
+
+
+}
--- src/ch/guggisberg/stefan/groupfitness/services/LoginBean.java
+++ src/ch/guggisberg/stefan/groupfitness/services/LoginBean.java
@@ -2,16 +2,15 @@ package ch.guggisberg.stefan.groupfitness.services;
 
 import java.io.Serializable;
 
-import javax.annotation.Resource;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.transaction.UserTransaction;
 
-
+import org.apache.log4j.Logger;
 
 /**
  * 
@@ -24,18 +23,18 @@ import javax.transaction.UserTransaction;
 @SessionScoped
 public class LoginBean implements Serializable  {
 	private static final long serialVersionUID = -3070846601605974845L;
-	
+	private static Logger log = Logger.getLogger(LoginBean.class);
+
 	// DB Verbindung
 	@PersistenceContext
 	private EntityManager em;
-	@Resource
-	private UserTransaction ut;
-	
+
+
 	// Logindaten
 	private String  email;
 	private String pass;
 	private boolean loggedIn;
-	
+
 	/**
 	 * Check the login Data
 	 * @return
@@ -43,27 +42,21 @@ public class LoginBean implements Serializable  {
 	public void checkLogin(){
 		Integer result=0;
 		Query query = em.createNativeQuery("Select Userid from groupfitness.user u WHERE u.UserEmail='" + email + "' AND u.UserPassword='" + pass + "'");
-		//Query query = em.createNativeQuery("Select Userid from groupfitness.user u WHERE UserEmail='stefan@gmx.ch' AND u.UserPassword='123';");
-
-		System.out.println("check");
 		try {
 			result =  (Integer) query.getSingleResult();
-			System.out.println("query ausgeführt");
 			if (result.intValue()>0){
 				//user = em.find(User2.class, result.longValue());
-				System.out.println("Logged in");
+				log.info("User mit Email " + email + "hat korrekt eingeloggt");
 				loggedIn=true;
-			
 			}
 		} catch (NoResultException e) {
 			loggedIn=false;
-			System.out.println("not logged in");
-		
+			log.info("User mit Email " + email + "hat ist nicht eingeloggt");
 		}
 
-		
+
 	}
-	
+
 	// Getter and Setter
 	public String getEmail() {
 		return email;
@@ -85,6 +78,8 @@ public class LoginBean implements Serializable  {
 	public void setLoggedIn(boolean loggedIn) {
 		this.loggedIn = loggedIn;
 	}
-	
 
+	public boolean isLogged(){
+		return loggedIn;
+	}
 }
